10e3e2c07867822ed82abd48ffd908c829bb28e7
acedia7 <1335357913@qq.com>
2024-11-10 18:54:48 +0800

    update

--- mantravel/man_you/bills/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Expense
-from .models import Budget
-admin.site.register(Expense)
-admin.site.register(Budget)
--- mantravel/man_you/memo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Memo
-
-class MemoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Memo
-        fields = '__all__'
